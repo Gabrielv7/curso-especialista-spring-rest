9e01be6f6337a7f1d1102d7594e7676bd4cd53ff
Gabriel Costa Afonso <gabriel.afonso@pampa.compasso>
2022-02-03 11:17:43 -0300

    5.6. Criando consultas com query methods

--- src/main/java/com/example/esr/api/controller/TesteController.java
+++ src/main/java/com/example/esr/api/controller/TesteController.java
@@ -1,9 +1,14 @@
 package com.example.esr.api.controller;
 
+import com.example.esr.domain.model.Cozinha;
 import com.example.esr.domain.repository.CozinhaRepository;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/teste")
@@ -15,12 +20,20 @@ public class TesteController {
         this.cozinhaRepository = cozinhaRepository;
     }
 
-    /*
+
     @GetMapping("/cozinhas/por-nome")
     public List<Cozinha> cozinhasPorNome(@RequestParam("nome") String nome) {
 
-        return cozinhaRepository.consultarPorNome(nome);
+        return cozinhaRepository.findListByNome(nome);
 
     }
-     */
+
+
+    @GetMapping("/cozinhas/por-nome-unico")
+    public Optional<Cozinha> cozinhaPorNomeUnico(@RequestParam("nome") String nome) {
+
+        return cozinhaRepository.findByNome(nome);
+
+    }
+
 }
