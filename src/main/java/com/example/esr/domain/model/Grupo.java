aeda4ef4ab100568369163ee2e8df5cb6d5d3f61
Gabriel Costa Afonso <gabriel.afonso@pampa.compasso>
2022-02-07 16:26:13 -0300

    6.9. Desafio: mapeando relacionamentos muitos-para-muitos

--- src/main/java/com/example/esr/domain/model/Grupo.java
+++ src/main/java/com/example/esr/domain/model/Grupo.java
@@ -4,35 +4,33 @@ import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import java.math.BigDecimal;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import java.util.List;
 
 @Getter
 @Setter
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Entity
-public class Produto {
+public class Grupo {
 
     @EqualsAndHashCode.Include
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
     private String nome;
 
-    private String descricao;
-
-    private BigDecimal preco;
-
-    private Boolean ativo;
-
-    @ManyToOne
-    @JoinColumn(nullable = false)
-    private Restaurante restaurante;
-
+    @ManyToMany
+    @JoinTable(name = "grupo_permissao",
+    joinColumns = @JoinColumn(name = "grupo_id"),
+    inverseJoinColumns = @JoinColumn(name = "permissao_id"))
+    private List<Permissao> permissoes;
 }
